8649bcd0e3c24212348e53e6f730899eefa578a4
ollasmyke <ollasmyke@gmail.com>
2018-02-01 14:41:15 +0100

    Late Commit

--- src/main/java/com/ollas/testspring/web/HelloWorld.java
+++ src/main/java/com/ollas/testspring/web/HelloWorld.java
@@ -1,12 +1,13 @@
 package com.ollas.testspring.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/value")
     public String hello(){
         return "Hello World!";
     }
